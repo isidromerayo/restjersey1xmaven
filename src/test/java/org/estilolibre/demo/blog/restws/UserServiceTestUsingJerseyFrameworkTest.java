3c04d1d9cc37a91f2e2a69255308d45b8ba29f1e
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2016-01-22 09:39:51 +0100

    Change to use jersey-test-framework-grizzly2 with this way no need launch application server (integration test)

--- src/test/java/org/estilolibre/demo/blog/restws/UserServiceTestUsingJerseyFrameworkTest.java
+++ src/test/java/org/estilolibre/demo/blog/restws/UserServiceTestUsingJerseyFrameworkTest.java
@@ -12,8 +12,9 @@ import org.codehaus.jettison.json.JSONObject;
 import org.junit.Test;
 
 import com.sun.jersey.api.client.UniformInterfaceException;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.test.framework.AppDescriptor;
+import com.sun.jersey.api.core.ClassNamesResourceConfig;
+import com.sun.jersey.api.core.DefaultResourceConfig;
+import com.sun.jersey.spi.container.servlet.WebComponent;
 import com.sun.jersey.test.framework.JerseyTest;
 import com.sun.jersey.test.framework.WebAppDescriptor;
 
@@ -22,19 +23,30 @@ import com.sun.jersey.test.framework.WebAppDescriptor;
  *
  */
 public class UserServiceTestUsingJerseyFrameworkTest extends JerseyTest {
-	private static final String LITERAL_URI_USER_INFO_SERVICE = "/RestJersey/rest/UserInfoService/";
+	private static final String LITERAL_URI_USER_INFO_SERVICE = "/UserInfoService/";
+
+	public static class AppConfig extends DefaultResourceConfig {
+		public AppConfig() {
+			super(UserInfo.class);
+		}
+	}
 
 	@Override
-	protected AppDescriptor configure() {
-		return new WebAppDescriptor.Builder().build();
+	public WebAppDescriptor configure() {
+		return new WebAppDescriptor.Builder()
+				.initParam(WebComponent.RESOURCE_CONFIG_CLASS,
+						AppConfig.class.getName())
+				.initParam(ClassNamesResourceConfig.PROPERTY_CLASSNAMES,
+						UserInfo.class.getName()).build();
 	}
 
 	@Test
 	public void testUserFetchesSuccess() throws JSONException,
 			URISyntaxException {
-		WebResource webResource = client().resource("http://localhost:8080/");
-		JSONObject json = webResource.path(
+
+		JSONObject json = resource().path(
 				LITERAL_URI_USER_INFO_SERVICE + "id/12").get(JSONObject.class);
+
 		assertEquals("12", json.get("id"));
 		assertEquals("Tim", json.get("firstName"));
 		assertEquals("Tester", json.get("lastName"));
@@ -43,8 +55,16 @@ public class UserServiceTestUsingJerseyFrameworkTest extends JerseyTest {
 
 	@Test(expected = UniformInterfaceException.class)
 	public void testUserNotFound() {
-		WebResource webResource = client().resource("http://localhost:8080/");
-		webResource.path(LITERAL_URI_USER_INFO_SERVICE + " id/666").get(
+
+		resource().path(LITERAL_URI_USER_INFO_SERVICE + " id/666").get(
 				JSONObject.class);
+	}
+
+	@Test
+	public void testUserAgeFetchesSuccessStringXML() {
+		String expected = "<User><Age>12</Age></User>";
+		String result = resource().path(
+				LITERAL_URI_USER_INFO_SERVICE + "age/12").get(String.class);
+		assertEquals(expected, result);
 	}
 }
